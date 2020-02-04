517ed0d75dcdb0bf58536f3db0e86788024a8c1a
Daniel Bolivar <daboliva@gmail.com>
2020-02-04 13:25:41 +0100

    Chapter 2 - Fib

---
+++ src/main/scala/chapter2/chapter2.scala
@@ -0,0 +1,16 @@
+package chapter2
+
+object chapter2 {
+
+  def fib(n: Int): Int = {
+    if (n <= 1) {
+      1
+    } else {
+      fib(n - 1) + fib(n - 2)
+    }
+
+  }
+
+  def main(args: Array[String]): Unit =
+    println(fib(5))
+}
