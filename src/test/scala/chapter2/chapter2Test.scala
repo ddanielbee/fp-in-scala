5b0df886b49893e0f26fe3dcc4b1f7adcde5840e
Daniel Bolivar <daboliva@gmail.com>
2020-02-14 13:39:19 +0100

    Add the rest of chapter 2 exercises

--- src/test/scala/chapter2/chapter2Test.scala
+++ src/test/scala/chapter2/chapter2Test.scala
@@ -26,4 +26,28 @@ class chapter2Test extends AnyFlatSpec with Matchers {
     chapter2.isSorted(List(1, 2, 3), (a: Int, b: Int) => a < b) shouldBe true
   }
 
+  behavior of "curry"
+
+  it should "have the exact same result for the curried & uncurried versions" in {
+    def uncurried(a: Int, b: Int) = a * b
+    val curried = chapter2.curry(uncurried)
+    uncurried(2, 3) shouldBe curried(2)(3)
+  }
+
+  behavior of "uncurry"
+
+  it should "have the exact same result for the curried & uncurried versions" in {
+    def curried(a: Int) = (b: Int) => a * b
+    val uncurried = chapter2.uncurry(curried)
+    uncurried(2, 3) shouldBe curried(2)(3)
+  }
+
+  behavior of "compose"
+
+  it should "have the same result for the composed version as calling the functions in order" in {
+    def fn1(a: Int) = a * 2
+    def fn2(c: Int) = c + 5
+    chapter2.compose(fn1, fn2)(2) shouldBe fn1(fn2(2))
+  }
+
 }
